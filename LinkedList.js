7b2d7dbc979f0d804cb58e9d38457d0f178207d6
bananabread08 <rmkfermin@gmail.com>
2022-08-06 17:28:37 +0800

    change imports to require
    create append(), prepend(), size(), getHead(), getTail, at()

--- LinkedList.js
+++ LinkedList.js
@@ -1,18 +1,67 @@
-import Node from './Node';
+const Node = require('./Node');
 
 class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
 
+  getHead() {
+    return this.head;
+  }
+
+  getTail() {
+    let tail = this.head;
+    while (tail.next !== null) tail = tail.next;
+    return tail;
+  }
+
+  size() {
+    let count = 0;
+    let pointer = this.head;
+    while (pointer !== null) {
+      count++;
+      pointer = pointer.next;
+    }
+    return count;
+  }
+
   append(value) {
-    const newNode = new Node(value);
+    // empty Node
     if (this.head === null) {
-      this.head = newNode;
+      this.head = new Node(value);
       return this;
     }
-    let tail = this.head;
-    while (newNode.next !== null) tail = newNode.next;
+    let tail = this.getTail();
+    // let tail = this.head;
+    // while (tail.next !== null) tail = tail.next;
     tail.next = new Node(value);
+    return tail;
+  }
+
+  prepend(value) {
+    if (this.head === null) {
+      this.head = new Node(value);
+      return this;
+    }
+    const prevHead = this.head;
+    this.head = new Node(value, prevHead);
+  }
+
+  at(index) {
+    let pointer = this.head;
+    for (let i = 0; i < index; i++) {
+      pointer = pointer.next;
+    }
+    return pointer ? pointer : null;
   }
 }
+
+const list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(4);
+
+console.log(list.size());
+console.log(list.getHead());
+console.log(list.getTail());
+console.log(list.at(0));
